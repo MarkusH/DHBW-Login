8a42692a7d59e0318cd867ed4614376d78914ab2
fbausch <studium@fbausch.de>
2011-02-03 22:23:11 +0100

    Use zenity to show informations to the user, write less information into log file. Check if there is a connection to the login server.

--- dhbwlogin.sh
+++ dhbwlogin.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-##############################################################################
+###########################################################################
 # This script makes a login at the DHBW WLAN more easy
 # Copyright (C) 2011  Markus Holtermann
 # 
@@ -36,11 +36,11 @@
 #  With these parameters you can change the behaviour of the script:
 #
 #  Your username for DHBWWebAuth:
-username=xxx
+username=xxxxx
 #
 #  Your password (because it is stored in plain text
 #  never use it for other purposes):
-password=xxx
+password=xxxxx
 #
 #  The internet address you want to check your login status with:
 pingaddress=google.com
@@ -49,30 +49,61 @@ pingaddress=google.com
 #  (Write s for seconds, m for minutes and h for hours.)
 sleeptime=15s
 #
+#  Where shall the log be stored?
+logfile="`dirname \"$0\"`/dhbwlogin.log"
+#
 ###########################################################################
 #
 #  The script:
 
-echo "DHBW Autologin Script"
-echo "You want to log in as $username."
-echo "I'll check in intervals of $sleeptime for connections to $pingaddress."
+workdir="`dirname \"$0\"`";
+echo "DHBW Autologin Script";
+echo "You want to log in as $username.";
+echo "I'll check in intervals of $sleeptime for connections to $pingaddress.";
+exec 3> >(zenity --notification --window-icon=$workdir/dhbwicon.ico --listen);
+echo "tooltip:DHBW-Autologin – verbunden" >&3;
+echo -e "\n\n---\n`date`: Start script" >> $logfile;
 
-while [ "something" ]
+setConnected () {
+	echo "icon:$workdir/dhbwicon.ico" >&3;
+	echo "tooltip:DHBW-Autologin – verbunden" >&3;
+}
+setDisconnected () {
+	echo "icon:$workdir/dhbwicon-grey.ico" >&3;
+	echo "tooltip:DHBW-Autologin – nicht verbunden" >&3;
+}
+
+while(true)
 do
-	ping -c 1 $pingaddress > /dev/null 2>&1
+	ping -c 1 https://dhbwwebauth.dhbw-mannheim.de/login.html > /dev/null 2>&1;
+	if [ $? -ne 0 ]
+	    then
+		setDisconnected;
+		echo "`date`: Login server not found."
+		sleep $sleeptime;
+		continue;
+	fi
+	ping -c 1 $pingaddress > /dev/null 2>&1;
 	if [ $? -eq 0 ]
 	    then
-		echo "`date`: You're logged in."
+		setConnected;
+		echo "`date`: You're logged in.";
 	    else
 		echo "`date`: You're not logged in."
-		echo "`date`: Trying to log in."
-		wget -T 5 -t 1 -O /dev/null --post-data 'buttonClicked=4&redirect_url=&err_flag=&info_flag=&info_msg=&username='$username'&password='$password'&Submit=Anmelden' "https://dhbwwebauth.dhbw-mannheim.de/login.html" > /dev/null 2>&1
+		setDisconnected;
+		echo "`date`: Trying to log in.";
+		wget -T 5 -t 1 -O /dev/null --post-data 'buttonClicked=4&redirect_url=&err_flag=&info_flag=&info_msg=&username='$username'&password='$password'&Submit=Anmelden' "https://dhbwwebauth.dhbw-mannheim.de/login.html" > /dev/null 2>&1;
 		if [ $? -eq 0 ]
 		    then
-			echo "`date`: Successfully logged in."
+			echo "`date`: Successfully logged in.";
+			setConnected;
+			echo "message:Im DHBW-WLAN angemeldet" >&3;
+			echo "`date`: Could connect to DHBWWebAuth." >> $logfile;
 		    else
-			echo "`date`: Failed to log in. Try again in $sleeptime."
+			echo "`date`: Failed to log in. Try again in $sleeptime.";
+			setDisconnected;
+			echo "message:Fehler beim Anmelden im DHBW-WLAN." >&3;
 		fi
 	fi
-	sleep $sleeptime
+	sleep $sleeptime;
 done
